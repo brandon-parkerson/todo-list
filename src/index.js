b6d3650a697ab0debc182e34ffe5cf9dfeb0ca7c
brandon-parkerson <brandon-m-parkerson@gmail.com>
2024-07-26 13:14:16 +0300

    Add some design to project cards on page.

--- src/index.js
+++ src/index.js
@@ -6,5 +6,16 @@ const sideBar = document.querySelector(".side-bar");
 document.addEventListener('DOMContentLoaded', () => {
     const img = document.querySelector('img');
     img.src = businessLogo;
+
+    makeCard();
     
   });
+
+
+function makeCard() {
+  const card = document.querySelector("#card1");
+  card.innerText = `Project One: content jfjddjffjd
+  jflsdjfffsfsdfsdf
+  fjsdfsdffd`;
+  
+}
